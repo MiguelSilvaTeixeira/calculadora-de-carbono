19086bc2b55455d27382fdfed4b420db776ac6dd
Miguel Teixeira <teixeira.miguel.trabalho@gmail.com>
2024-10-29 16:12:28 -0300

    feat(funcoes.py): definir fatores de emissão para diferentes atividades

---
+++ funcoes.py
@@ -0,0 +1,12 @@
+# Definindo os fatores de emissão para diferentes atividades em kg de CO2 por unidade
+fatoresEmissao = {
+    "eletricidade": 0.233,  # kg CO2 por kWh
+    "gasolina": 2.31,       # kg CO2 por litro de gasolina
+    "diesel": 2.68,         # kg CO2 por litro de diesel
+    "voo_domestico": 0.255, # kg CO2 por km em voo doméstico
+    "voo_internacional": 0.150, # kg CO2 por km em voo internacional
+    "transporte_publico": 0.105, # kg CO2 por km por pessoa (média)
+    "residuos_solidos": 1.42      # kg CO2 por kg de resíduos sólidos (média)
+}
+
+# TODO: Implementar funcionalidades adicionais aqui.
